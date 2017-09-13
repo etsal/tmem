89764a6ad65f418ef300edb3578490304e079052
Aimilios Tsalapatis <aimilios.tsalapatis@gmail.com>
2017-09-13 12:39:46 +0300

    fixed up kvm backend

--- tmem_kvm.c
+++ tmem_kvm.c
@@ -18,45 +18,53 @@
 #define TMEM_POOL_SIZE (64 * 1024 * 1024) 
 
 
+static u64 current_memory; 
+
 static struct page *control_page = NULL;
 
-void create_control_page(struct tmem_key tmem_key, size_t len) {
+void create_control_page(struct tmem_key tmem_key, struct page *page, size_t len)
+{
 	struct key_value key_value;
 
 	key_value.key = tmem_key.key;
-	key_value.key_len = tmem_key.keylen;
+	key_value.key_len = tmem_key.key_len;
+	key_value.value = page_address(page);
 	key_value.value_len = len;
 
-	memcpy(page, &key_value, sizeof(key_value));
-	
-
-}
-
-int tmem_kvm_put_page(struct page *page, struct tmem_key tmem_key, size_t len) {
-	
-	create_control_page(tmem_key, len);
-	kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_PUT_OP, page_to_pfn(*page), page_to_pfn(*control_page));
-
+	memcpy(page_address(control_page), &key_value, sizeof(key_value));
+	memcpy(page_address(control_page) + sizeof(key_value), key_value.key, key_value.key_len);
 	
 }
 
-int tmem_kvm_get_page(struct page *page, struct tmem_key tmem_key, size_t *len) {
-
-	create_control_page(tmem_key, *len);
-	kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_GET_OP, page_to_pfn(*page), page_to_pfn(*control_page));
-
+int tmem_kvm_put_page(struct page *page, struct tmem_key tmem_key, size_t len)
+{
+	
+	create_control_page(tmem_key, page, len);
+	return kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_PUT_OP, page_to_pfn(control_page), page_to_pfn(page));
 }
 
-void tmem_kvm_invalidate_page(struct tmem_key tmem_key) {
+int tmem_kvm_get_page(struct page *page, struct tmem_key tmem_key, size_t *len)
+{
+	int ret;
 
-	create_control_page(tmem_key, *len);
-	kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_INVALIDATE_OP, page_to_pfn(*page), page_to_pfn(*control_page));
+	create_control_page(tmem_key, page, *len);
+	ret = kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_GET_OP, page_to_pfn(control_page), page_to_pfn(page));
+	*len = *((size_t *) page_to_virt(control_page));
+
+	return ret;
+}
+
+void tmem_kvm_invalidate_page(struct tmem_key tmem_key)
+{
+
+	create_control_page(tmem_key, NULL, 0);
+	kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_INVALIDATE_OP, page_to_pfn(control_page), 0);
 
 }
 
 void tmem_kvm_invalidate_area(void) {
 
-	create_control_page(tmem_key, *len);
+//	create_control_page(tmem_key, *len);
 //	kvm_hypercall3(KVM_HC_TMEM, PV_TMEM_INVALIDATE_OP, page_to_pfn(*page), page_to_pfn(*control_page));
 
 }
@@ -90,9 +98,11 @@ static int __init tmem_kvm_init(void)
 	if (!debugfs_create_u64("current_memory", S_IRUGO, root, &current_memory)) 
 		pr_err("debugfs entry could not be set up\n");
 
+	return 0;
+
 out:
 	if (control_page)
-		free_page(control_page);
+		free_page((long) control_page);
 
 	return 0;
     
